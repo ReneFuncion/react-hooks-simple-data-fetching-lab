418a8818dde734375fe5ce99f69edc3acea8b1a4
Rene Funcion <rcfun959@gmail.com>
2023-06-11 01:09:05 -0700

    Completed react-hooks-simple-data-fetching-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => {
+        if (!r.ok) {
+          throw new Error("Network error");
+        }
+        return r.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setDogImage(data.message);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {dogImage ? <img src={dogImage} alt="A Random Dog" /> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default App;
